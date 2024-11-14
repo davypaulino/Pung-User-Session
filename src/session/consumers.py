764f2304f4781318aa0e360a7cf6afbbdde97395
beatrizyoshimori <103694028+beatrizyoshimori@users.noreply.github.com>
2024-11-14 01:55:48 +0000

    ws

--- src/session/consumers.py
+++ src/session/consumers.py
@@ -4,8 +4,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class RoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_code = self.scope['url_route']['kwargs']['room_code']
-        self.room_group_name = f"room_{self.room_code}"
+        self.room_name = self.scope['url_route']['kwargs']['room_code']
+        self.room_group_name = f"room_{self.room_name}"
 
         await self.channel_layer.group_add(
             self.room_group_name,
