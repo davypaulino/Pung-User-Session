06d4988644ccf497227854734e2b9a040f4459bf
beatrizyoshimori <103694028+beatrizyoshimori@users.noreply.github.com>
2024-11-20 19:45:16 +0000

    feat: added player color

--- src/players/models.py
+++ src/players/models.py
@@ -1,12 +1,24 @@
 import uuid
 
 from django.db import models
+from enum import Enum
+
+class playerColors(Enum):
+    RED = 0
+    BLUE = 1
+    GREEN = 2
+    YELLOW = 3
 
 class Player(models.Model):
     id = models.CharField(primary_key=True, max_length=64, editable=False)
     name = models.CharField(max_length=100)
     roomCode = models.CharField(max_length=64)
-    profileColor = models.IntegerField(default=0)
+    profileColor = models.IntegerField(choices=[
+        (1, "Red"),
+        (2, "Blue"),
+        (3, "Green"),
+        (4, "Yellow"),
+    ], default=0)
     urlProfileImage = models.CharField(max_length=100)
     status = models.BooleanField(default=True)
     createdBy = models.CharField(max_length=64)
