6381cd2cf7059b186012e46d62ea161e30a859df
beatrizyoshimori <103694028+beatrizyoshimori@users.noreply.github.com>
2024-12-20 14:10:20 +0000

    feat: added tournament view

--- src/games/views.py
+++ src/games/views.py
@@ -4,7 +4,7 @@ import json
 from django.http import HttpResponse
 from django.views import View
 from rooms.models import Room, Match
-from rooms.models import RoomStatus
+from rooms.models import RoomStatus, roomTypes
 from players.models import Player, MatchPlayer
 
 redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
@@ -14,15 +14,15 @@ class GameView(View):
         user_id = request.headers.get('X-User-Id')
         if user_id is None:
             return HttpResponse(f"User ID not found", status=400)
+
         room = Room.objects.filter(code=room_code).first()
-        
         if room is None:
             return HttpResponse(f"Room {room_code} not found", status=400)
         if room.players.count() < 2:
             return HttpResponse(f"Minimal amount of players {2}", status=403)
         if room.createdBy != user_id:
             return HttpResponse(f"User {user_id} is not the owner of room {room_code}", status=403)
-        if room.type == 1:
+        if room.type == roomTypes.TOURNAMENT.value:
             return HttpResponse(f"Room {room_code} is not a match game room", status=403)
 
         room.status = RoomStatus.CREATING_GAME
@@ -57,6 +57,50 @@ class GameView(View):
                     "color": player.profileColor,
                 }
                 for player in room.players.all()
+            ]
+        }
+
+        redis_client.rpush("create-game-queue", json.dumps(message))
+        return HttpResponse(f"Game created for room {room_code}", status=201)
+
+class TournamentGameView(View):
+    def post(self, request, room_code):
+        user_id = request.headers.get('X-User-Id')
+        if user_id is None:
+            return HttpResponse(f"User ID not found", status=400)
+
+        room = Room.objects.filter(code=room_code).first()
+        if room is None:
+            return HttpResponse(f"Room {room_code} not found", status=400)
+        if room.players.count() < 4:
+            return HttpResponse(f"Minimal amount of players {4}", status=401)
+        if room.type != roomTypes.TOURNAMENT.value:
+            return HttpResponse(f"Room {room_code} is not a tournament game room", status=403)
+
+        player_one = Player.objects.filter(id=user_id).first()
+
+        matchPlayer = MatchPlayer.objects.filter(player=player_one).first()
+        match = matchPlayer.match
+        secondMatchPlayer = MatchPlayer.objects.filter(match=match).exclude(player=player_one).first()
+        player_two = secondMatchPlayer.player
+
+        message = {
+            "type": "create_game",
+            "roomId": room.id,
+            "matchId": match.id,
+            "isSinglePlayer": False,
+            "ownerId": user_id,
+            "players": [
+                {
+                    "id": player_one.id,
+                    "name": player_one.name,
+                    "color": player_one.profileColor,
+                },
+                {
+                    "id": player_two.id,
+                    "name": player_two.name,
+                    "color": player_two.profileColor,
+                }
             ]
         }
 
